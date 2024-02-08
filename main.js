0d8d4f74117d241852c2bef36300a2ff53df9ddf
Whitefox-SA <deiv320@gmail.com>
2024-02-08 20:26:07 +0100

    agregada ventana modal

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ function darkModeListener() {
 }
 
 document.querySelector("input[type='checkbox']#dark-toggle").addEventListener("click", darkModeListener);
+
+
+// Modal
+function toggleModal(modal_id) {
+  document.getElementById(modal_id).classList.toggle("hidden");
+  const sections = document.querySelectorAll("section");
+
+  // Itera a través de todas las etiquetas <section> y aplica la clase "blur" a cada una
+  sections.forEach(section => {
+  section.classList.toggle("blur-md");
+  });
+}
