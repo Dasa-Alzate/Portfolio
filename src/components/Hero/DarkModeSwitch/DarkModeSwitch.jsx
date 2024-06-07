7dcff98233bdebabd9b2f63d8a1a41015a4a9752
dasa-alzate <david.s356811@cesurformacion.com>
2024-06-07 21:02:38 +0200

    internacionalizacion componentes del hero section

--- src/components/Hero/DarkModeSwitch/DarkModeSwitch.jsx
+++ src/components/Hero/DarkModeSwitch/DarkModeSwitch.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const DarkModeSwitch = () => {
     const [darkMode, setDarkMode] = useState(true);
+    const { t, i18n } = useTranslation();
 
     useEffect(() => {
         if (darkMode) {
@@ -32,7 +34,7 @@ const DarkModeSwitch = () => {
                         <div className="block bg-teal-800/50 dark:bg-zinc-400 rounded-full w-14 h-8"></div>
                         <div className="top-1 left-1 absolute bg-teal-600/30 dark:bg-gray-500 rounded-full w-6 h-6 transition dot"></div>
                     </div>
-                    <p className="ml-3 text-gray-500 hover:text-gray-200 dark:text-white">Modo Oscuro</p>
+                    <p className="ml-3 text-gray-500 hover:text-gray-200 dark:text-white">{t('navbar.dark-mode')}</p>
                 </label>
             </div>
         </div>
